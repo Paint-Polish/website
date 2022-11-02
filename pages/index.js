e1d2cfd1b1ced7969faacbbff2f00f5270a7c3e3
Ed Oswald Go <edoswaldgo@gmail.com>
2022-11-02 11:32:33 +0800

    Update homepage to showcase image first

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = ({ inventoryData = [], categories: categoryData = [], trendingData 
         </Head>
         <div className="bg-gray-300
         p-6 pb-10 smpb-6
-        flex lg:flex-row flex-col">
+        flex lg:flex-row flex-col-reverse">
           <div className="pt-4 pl-2 sm:pt-12 sm:pl-12 flex flex-col">
             <Tag
               year="FEATURED PRODUCT"
